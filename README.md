0f48b73c08a31e4dddc226ad1c1d836d3f2137f1
DavidIoner <davidhioner@gmail.com>
2023-06-20 02:01:16 -0300

    working

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Obersvações
+Tesseract-OCR deve ser instalado e adiocionado ao PATH. Pode ser encontrtado em:
+https://github.com/tesseract-ocr/tesseract
--- conversor.py
+++ conversor.py
@@ -1,12 +1,14 @@
 import pytesseract
 from PIL import Image
 
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
 # Carregar a imagem do documento
 image_path = "texto.jpeg"
 image = Image.open(image_path)
 
 # Extrair o texto da imagem usando OCR
-extracted_text = pytesseract.image_to_string(image, lang="eng")
+extracted_text = pytesseract.image_to_string(image, lang="pt-br")
 
 # Salvar o texto extraído em um arquivo de saída
 output_file = "output.txt"
